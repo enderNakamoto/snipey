df9faa26674ac2b38bf9ba01340210987b87f3fc
enderNakamoto <ender.nakamoto@gmail.com>
2021-05-06 21:52:46 -0400

    telegram notification for quickswap added

--- quickswap.js
+++ quickswap.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const ethers = require('ethers');
 const addresses = require("./addresses");
 const abis = require('./abis');
+const axios = require('axios');
 
 const version = "0.1.3";
 
@@ -20,6 +21,11 @@ console.log("listening for pairs created");
 console.log("account is", account.address);
 
 factory.on('PairCreated', async (token0, token1, pairAddress) => {
-  console.log(`
-    pair Created: ${token0} ---AND---- ${token1} pairAddress is: ${pairAddress}`);
+  console.log(`pair Created: ${token0} ---AND---- ${token1} pairAddress is: ${pairAddress}`);
+  axios({
+    method: 'post',
+    url: process.env.TELEGRAM_QUICK_BOT_URL, 
+    params: {},
+    data: {"tokenA":token0,"tokenB":token1,"pair address": pairAddress}
+  })  
 });
