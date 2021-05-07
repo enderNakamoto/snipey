1cb2a6529d5ac43c1b388e4c60b0705282a65c85
enderNakamoto <ender.nakamoto@gmail.com>
2021-05-06 22:18:31 -0400

    added matic vigil endpoint

--- quickswap.js
+++ quickswap.js
@@ -8,7 +8,7 @@ const axios = require('axios');
 
 const version = "0.1.3";
 
-const provider = new ethers.providers.WebSocketProvider(process.env.MATIC_PUBLIC_WS_URL_5);
+const provider = new ethers.providers.WebSocketProvider(process.env.MATIC_VIGIL_WS_URL);
 const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
 const account = wallet.connect(provider);
 
