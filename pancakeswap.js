e5fe3351baf30e61c0b6f7cbf2e45f5ff10a261f
enderNakamoto <ender.nakamoto@gmail.com>
2021-05-06 21:40:04 -0400

    added telegram notification to pancakeswap pairs

--- pancakeswap.js
+++ pancakeswap.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-
 const ethers = require('ethers');
 const addresses = require("./addresses");
 const abis = require('./abis');
-
+const axios = require('axios');
+    
 const version = "0.1.3";
 
 const provider = new ethers.providers.WebSocketProvider(process.env.BSC_QUICKNODE_WS_URL);
@@ -20,6 +20,11 @@ console.log("listening for pairs created");
 console.log("account is", account.address);
 
 factory.on('PairCreated', async (token0, token1, pairAddress) => {
-  console.log(`
-    pair Created: ${token0} ---AND---- ${token1} pairAddress is: ${pairAddress}`);
+  console.log(`pair Created: ${token0} ---AND---- ${token1} pairAddress is: ${pairAddress}`);
+  axios({
+    method: 'post',
+    url: process.env.TELEGRAM_PANCAKE_BOT_URL, 
+    params: {},
+    data: {"tokenA":token0,"tokenB":token1,"pair address": pairAddress}
+  })
 });
