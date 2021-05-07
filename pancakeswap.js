6454f0b0a378f8c34757408bd5126fe21608864c
enderNakamoto <ender.nakamoto@gmail.com>
2021-05-06 20:32:56 -0400

    pancakeswap also added

--- pancakeswap.js
+++ pancakeswap.js
@@ -7,16 +7,17 @@ const abis = require('./abis');
 
 const version = "0.1.3";
 
-const provider = new ethers.providers.WebSocketProvider(process.env.MATIC_PUBLIC_WS_URL_5);
+const provider = new ethers.providers.WebSocketProvider(process.env.BSC_QUICKNODE_WS_URL);
 const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
 const account = wallet.connect(provider);
 
 const factory = new ethers.Contract(
-  addresses.MATIC.quickswapFactory, abis.uniFactory, account
+  addresses.BSC.pancakeFactory, abis.uniFactory, account
 );
 
 console.log("version", version);
 console.log("listening for pairs created");
+console.log("account is", account.address);
 
 factory.on('PairCreated', async (token0, token1, pairAddress) => {
   console.log(`
---
+++ quickswap.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+
+const ethers = require('ethers');
+const addresses = require("./addresses");
+const abis = require('./abis');
+
+const version = "0.1.3";
+
+const provider = new ethers.providers.WebSocketProvider(process.env.MATIC_PUBLIC_WS_URL_5);
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+const account = wallet.connect(provider);
+
+const factory = new ethers.Contract(
+  addresses.MATIC.quickswapFactory, abis.uniFactory, account
+);
+
+console.log("version", version);
+console.log("listening for pairs created");
+console.log("account is", account.address);
+
+factory.on('PairCreated', async (token0, token1, pairAddress) => {
+  console.log(`
+    pair Created: ${token0} ---AND---- ${token1} pairAddress is: ${pairAddress}`);
+});
